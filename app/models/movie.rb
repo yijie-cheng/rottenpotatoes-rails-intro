a18989b9a8a4b49f76d234334318d88efa2c576a
Yi-Jie Cheng <yc4708@columbia.edu>
2025-10-02 20:24:46 +0000

    Final double check

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,13 +1,13 @@
 class Movie < ActiveRecord::Base
-  RATINGS = %w[G PG PG-13 R].freeze
-
   def self.all_ratings
-    from_db = distinct.where.not(rating: [nil, ""]).order(:rating).pluck(:rating)
-    from_db.presence || RATINGS
+    ['G', 'PG', 'PG-13', 'R']
   end
 
   def self.with_ratings(ratings_list)
-    return all unless ratings_list.present?
-    where('LOWER(rating) IN (?)', Array(ratings_list).map { |r| r.to_s.downcase })
+    if ratings_list
+      where(rating: ratings_list)
+    else
+      all
+    end
   end
 end
